ca06fa828e8ccdeac9296ac87e7a26769baf0cc2
Naiara Rodrigues <rnaiara92@gmail.com>
2023-01-03 14:58:42 -0300

    todos

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+    res.render("../views/index");
+});
+
+app.listen(3001, function () {
+    console.log('Executando na porta 3001');
+});
